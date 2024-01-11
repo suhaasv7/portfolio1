e2618faeb2e2e62af8c1942147557b07943d124a
suhaasvijay <suhaasvijay7@gmail.com>
2024-01-11 15:47:31 +0530

    made webpage responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,33 +2,33 @@ const links = "text-black hover:text-orange-600"
 
 export default function Footer() {
   return (
-    <div className="flex justify-between mb-10 mt-28 max-w-4xl mx-auto items-center">
+    <div className="flex flex-col items-center justify-between max-w-6xl gap-10 px-5 pb-12 mx-auto mt-16 lg:px-20 lg:flex-row">
 
       <div>
         <p>
-          © 2023 Suhaas Vijay. All rights reserved.
+          © 2024 Suhaas V. All rights reserved.
         </p>
         <p>
-          Made with ❤️ by Suhaas Vijay
+          Made with ❤️ by Suhaas V
         </p>
       </div>
 
-      <div className="p-2 border-2 border-orange-400 w-fit h-fit cursor-pointer">
+      <div className="p-2 text-white bg-orange-500 rounded-md cursor-pointer w-fit h-fit">
         Contact Me
       </div>
 
-      <div className="flex flex-col space-y-1 ">
+      {/* <div className="flex flex-col space-y-1 ">
         <a href="#" className={links}> Home</a>
         <a href="#"> About</a>
         <a href="#"> Skills</a>
         <a href="#"> Blogs</a>
       </div>
 
-      <div className='flex space-y-2 items-center flex-col'>
+      <div className='flex flex-col items-center space-y-2'>
         <a href='#'><img src='/icons/instagram.svg' alt='instagram' className='w-8' /></a>
         <a href='https://www.linkedin.com/in/suhaasv7/' target='blank'><img src='/icons/linkedin.svg' alt='linkedin' className='w-8' /></a>
         <a href='https://github.com/suhaasvijay' target='blank'><img src='/icons/github.svg' alt='github' className='w-8' /></a>
-      </div>
+      </div> */}
 
     </div>
   )
--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -19,17 +19,21 @@ export default function MainSection() {
   return (
     <div>
 
-      <div className='flex mt-10 justify-between relative hero'>
-        <div className='my-auto ml-20'>
+      <div className='relative flex flex-col items-center justify-between px-5 mt-10 hero lg:flex-row'>
+        <div className='my-auto lg:ml-20'>
           <h1 className='text-4xl font-bold'>Hello</h1>
           <p className='text-[55px] font-bold text-gray-800'>I&apos;m <span className='text-orange-600' ref={el}>Full stack Developer</span></p>
-          <p className='w-[500px] text-sm text-justify'>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum, expedita fugit consequatur commodi eaque quisquam, delectus nam voluptatum assumenda veniam, quos magnam quam rem recusandae at fuga voluptatem sit. Doloremque.
+          <p className='max-w-[500px] text-sm text-justify flex flex-wrap '>
+            I'm an experienced full-stack developer with 1.5 years of expertise in ReactJS, Tailwind, MySQL, and more. Proficient in front-end and back-end development, I'm dedicated to continuous improvement, currently expanding my skills in Next.js, AWS, and mobile development. Let's collaborate to elevate your digital projects with expertise and innovation.
           </p>
         </div>
-        <img className='w-[45%] absolute right-0 -bottom-20 splash' src='/splash.svg' />
-        <img className='w-[20%] absolute -right-40 -top-10 splash' src='/blob.svg' />
-        <img className='profile w-[45%]' src='/profile-bg.png' />
+        <div>
+          <img className='w-[45%] absolute right-0 -bottom-20 splash hidden lg:block opacity-60 blur-2xl' src='/splash.svg' />
+          {/* <img className='w-[20%] absolute -right-40 -top-10 splash hidden lg:block' src='/blob.svg' /> */}
+          <div className='container'>
+            <img className='profile' src='/profile-bg.png' />
+          </div>
+        </div>
       </div>
 
     </div>
